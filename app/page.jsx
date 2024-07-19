bbd4fbe2e45fe839fcfec9b38030a7966b651a60
TVLJ Solutions <viswarajfc5@gmail.com>
2024-07-19 15:40:03 +0530

    new commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
-import Navbar from "./home/navbar";
-import WrapperVP from "./home/wrapperVP";
-
+import Navbar from "./home/Navbar";
+import WrapperVP from "./home/WrapperVP";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-2 bg-gradient-to-b from-[#101115] to-[#171819] text-white">
