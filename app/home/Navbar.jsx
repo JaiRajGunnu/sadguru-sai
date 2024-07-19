44726b84773424129f00366126167bd1545af832
TVLJ Solutions <viswarajfc5@gmail.com>
2024-07-19 15:32:06 +0530

    new commit

--- app/home/Navbar.jsx
+++ app/home/Navbar.jsx
@@ -5,7 +5,16 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="flex justify-center w-full font-poppins p-0 md:p-2 rounded-b-lg bg-[#101115]">
-        <Link href={"./"}><img className="w-[150px] m-5 items-center justify-center" src="/tvljsol.png"/></Link>
+      <Link href="./">
+        <div className="w-[150px] m-5 flex items-center justify-center">
+          <Image
+            src="/tvljsol.png"
+            alt="Logo"
+            width={150}
+            height={150} // Adjust based on the aspect ratio of your image
+          />
+        </div>
+      </Link>
     </nav>
   );
 };
