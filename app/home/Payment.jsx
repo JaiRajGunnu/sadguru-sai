68f2fe6dd2454450771ddb25486f5825d50dba99
TVLJ Solutions <viswarajfc5@gmail.com>
2024-07-19 15:53:17 +0530

    file change

--- app/home/Payment.jsx
+++ app/home/Payment.jsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import MyCarousel from './MyCarousel';
+import MyCarousel from './myCarousel';
 import { Tv } from 'lucide-react';
 
 
