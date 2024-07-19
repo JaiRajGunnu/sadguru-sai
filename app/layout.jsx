91a92f0fc6ade6d54ef40bcff11c8cdb09cf6976
TVLJ Solutions <viswarajfc5@gmail.com>
2024-07-19 22:01:45 +0530

    MetaChange

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,9 @@ import { poppins,hso,nst } from "@/utils/font";
 export const metadata = {
   title: "Sadguru Sai",
   description: "This year marks the 100-year completion of a momentous event of Sai Baba taking Samadhi in Shirdi. In last 100 years, his teachings continue to comfort millions of followers worldwide. ",
+  verification: {
+    google: 'OZSdOBRXuwwqeM1Pz-L4GFy4tW0axfnrBNfjFN6wC0o',
+  },
 };
 
 export default function RootLayout({ children }) {
