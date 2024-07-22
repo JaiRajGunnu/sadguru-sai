d5fa4577f133dde3aa3ca990844bd71b2255343d
TVLJ Solutions <viswarajfc5@gmail.com>
2024-07-22 11:44:14 +0530

    video

--- app/payment/Video.jsx
+++ app/payment/Video.jsx
@@ -5,7 +5,7 @@ const Video = () => {
     <div className="flex items-center justify-center rounded-2xl flex-col md:gap-1">
       <iframe
         title="vimeo-player"
-        src="https://player.vimeo.com/video/851958974?h=244ca2fef7"
+        src="https://player.vimeo.com/video/988252174?h=244ca2fef7"
         className="h-[260px] w-full md:h-[380px] lg:h-[495px] "
       ></iframe>
       <div className="w-full h-full p-3 md:p-7 lg:p-5  font-poppins flex items-center justify-center lg:justify-around">
