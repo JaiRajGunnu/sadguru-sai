94a0cf8bfbdba3c00ebc3633cd9f6c4e38133ed8
TVLJ Solutions <viswarajfc5@gmail.com>
2024-08-06 19:36:14 +0530

    pop

--- app/payment/Modal.jsx
+++ app/payment/Modal.jsx
@@ -30,26 +30,36 @@ export default function Modal() {
           </Button>
         </DialogTrigger>
         <DialogContent
-          className={`text-base lg:max-w-[1000px] bg-[#0f1014] rounded-xl text-white border-none p-8 tracking-wide flex flex-col items-center justify-center w-[95%] md:w-[75%] ${
-            showVideo ? "lg:h-[70%]" : "md:h-[75%] lg:h-[55%]"
+          className={`text-base lg:max-w-[1000px] bg-[#0f1014] rounded-xl text-white border-none p-8 tracking-wide flex flex-col items-center justify-center w-[95%] md:w-[80%] md:h-[80%] ${
+            showVideo ? "lg:h-[80%]" : " lg:h-[60%]"
           }`}
         >
           {showVideo ? (
-            <div className="flex items-center justify-center rounded-2xl flex-col">
-              <iframe
+            <div className="flex justify-center rounded-2xl flex-col">
+              <DialogHeader className=" flex items-center md:items-center lg:items-start md:ml-0 lg:ml-[5%]">
+                <DialogTitle className="text-2xl font-hso font-medium">
+                Watch Video
+                </DialogTitle>
+                <DialogDescription className=" font-poppins font-extralight text-sm  ">
+                The demonstration video for completing the subscription payment.
+                </DialogDescription>
+              </DialogHeader>
+                <iframe
                 title="vimeo-player"
                 src="https://player.vimeo.com/video/994453004"
-                className="h-[250px] w-[375px] md:h-[380px] lg:w-[1025px] lg:h-[400px]"
+                className="h-[250px] w-[375px] md:h-[380px] md:w-[450px] lg:w-[1025px] lg:h-[400px] lg:mt-[2%]"
                 allowFullScreen
                 allow="fullscreen"
               ></iframe>
+              <div className="flex items-center justify-center">
               <Button
                 type="button"
-                className="bg-[#1f1f1f] mt-8"
+                className="bg-[#1f1f1f] mt-8 "
                 onClick={() => setShowVideo(false)}
               >
                 Back to Instructions
               </Button>
+              </div>
             </div>
           ) : (
             <>
@@ -57,14 +67,14 @@ export default function Modal() {
                 <DialogTitle className="text-2xl font-hso font-medium">
                   Instructions
                 </DialogTitle>
-                <DialogDescription className="font-poppins text-sm">
+                <DialogDescription className="font-poppins font-extralight  text-sm">
                   Follow these steps or watch video to make subscription payment:
                 </DialogDescription>
               </DialogHeader>
-              <ul className="list-disc list-inside font-poppins w-full m-2 space-y-1 flex flex-col items-start justify-start">
+              <ul className="list-disc list-inside font-poppins font-extralight  w-full m-2 space-y-1 flex flex-col items-start justify-start">
                 <li>
-                  Click on <strong>&quot;Pay ₹ 59&quot;</strong> or{" "}
-                  <strong>&quot;Pay ₹ 149&quot;</strong> button or this
+                  Click on <strong className="font-bold">&quot;Pay ₹ 59&quot;</strong> or{" "}
+                  <strong className="font-bold">&quot;Pay ₹ 149&quot;</strong> button or this
                   <Link
                     href="https://amzn.to/4d8hpQ8"
                     target="_blank"
@@ -76,7 +86,7 @@ export default function Modal() {
                 </li>
                 <li>
                   Enter the desired amount and select the option{" "}
-                  <strong>&quot;Buy now&quot;</strong>.
+                  <strong className="font-bold">&quot;Buy now&quot;</strong>.
                 </li>
                 <li>
                   Complete the payment using a UPI app or any other available
@@ -84,12 +94,16 @@ export default function Modal() {
                 </li>
                 <li>
                   After the payment is successful, look for the{" "}
-                  <strong>&quot;Share Link&quot;</strong> button.
+                  <strong className="font-bold">&quot;Share Link&quot;</strong> button.
                 </li>
-                <li>Share the link directly via apps or by copying the link.</li>
                 <li>
-                  Now, please send the generated Amazon link to us via Telegram or
-                  Instagram.
+                  Copy the link and then return to our website.
+                </li>
+                <li>
+                  Please click on <strong className="font-bold">&quot;Verify Payment&quot;</strong> and submit the details for vertification.
+                </li>
+                <li>
+                  After verification, you will receive the details shortly, and you will be able to access the content.
                 </li>
               </ul>
               <Button
@@ -100,7 +114,7 @@ export default function Modal() {
                <Play size={17} className="mr-2" />
                 Watch Video
               </Button>
-              <p className="opacity-50 text-white font-poppins text-center text-[12px] p-03 lg:w-[35%]">( Click the button above to watch the demonstration of the subscription process. )</p>
+              <p className="opacity-50 text-white font-poppins text-center text-[12px] font-light p-03 lg:w-[35%]">( Click the button above to watch the demonstration of the subscription process. )</p>
             </>
           )}
 
